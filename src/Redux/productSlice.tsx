90338628a38b28251f86cb2229acf2bc8d179e3d
rakib883 <sheikhrakib883@gmail.com>
2024-06-25 11:39:38 +0600

    push

--- src/Redux/productSlice.tsx
+++ src/Redux/productSlice.tsx
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface StateType {
   addData: any[];
   searchData: string;
-  cartData: Array<{ id: number; quentity: number }>;
+  cartData:any[];
 }
 
 const initialState: StateType = {
