60513aa08575dc1d1a94f03585f38e91d4dd0bf6
rakib883 <sheikhrakib883@gmail.com>
2024-07-09 01:43:48 +0600

    first

--- src/UI/CheickBanner.tsx
+++ src/UI/CheickBanner.tsx
@@ -3,7 +3,7 @@ import { MdOutlineMail } from "react-icons/md";
 
 function CheickBanner() {
   return (
-    <div className="bg-checkOut">
+    <div className="bg-checkBanner">
         <div className=" flex justify-center py-20 items-center flex-col gap-4">
            <div className="pragraph font-mainFont font-bold ">
                 <h1 className="text-[30px] text-black">Get <span className="text-amber-400">20%</span> Off Discount Coupon</h1>
